5d4e2dde733c603897936073f9398bf94ec5e7eb
dupinder dhiman <dupinder.dhiman@catalystone.com>
2017-04-07 00:01:00 +0530

    V1 MM: Submitting validating email address, send OTP and json accroding to email

--- src/main/java/authentications/VerifyEmail.java
+++ src/main/java/authentications/VerifyEmail.java
@@ -1,11 +1,15 @@
 package authentications;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
 
 import user.UserAuthentication;
 import utilities.StringTools;
@@ -13,42 +17,66 @@ import utilities.StringTools;
 /**
  * Servlet implementation class VerifyEmail
  */
-public class VerifyEmail extends HttpServlet 
-{
+public class VerifyEmail extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    private UserAuthentication userAuthentication = new UserAuthentication();
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public VerifyEmail() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	private UserAuthentication userAuthentication = new UserAuthentication();
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public VerifyEmail() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		return;
 		// TODO Auto-generated method stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		String email = request.getParameter("email");
-		if(StringTools.isValidString(email))
-		{
-			if(userAuthentication.generateOtpAndSendEmail(email))
-			{
-				response.getWriter().write("true");
-				return;
+		if (StringTools.isValidString(email)) {
+			try {
+
+				Map<String, String> userStatus = new HashMap<String, String>();
+				switch (UserAuthentication.IsExistingUser(email)) {
+				case UserAuthentication.ACTION_INVALID_USER:					
+					userStatus.put("isValidUser", "false");
+					response.getWriter().write(new Gson().toJson(userStatus));
+					break;
+				case UserAuthentication.ACTION_VALID_EXSISTING_USER:
+					userStatus.put("isValidUser", "true");
+					userStatus.put("isNewUser", "false");
+					response.getWriter().write(new Gson().toJson(userStatus));
+					break;
+				case UserAuthentication.ACTION_VALID_NEW_USER:
+					if (userAuthentication.generateOtpAndSendEmail(email)) {
+						userStatus.put("isValidUser", "true");
+						userStatus.put("isNewUser", "true");
+						response.getWriter().write(new Gson().toJson(userStatus));						
+					}
+					break;
+				default:
+					break;
+				}
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
+
 		}
-		
-		response.getWriter().write("false");
 	}
 
 }
--- src/main/java/user/UserAuthentication.java
+++ src/main/java/user/UserAuthentication.java
@@ -1,22 +1,32 @@
 package user;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.print.attribute.standard.PresentationDirection;
+
+import connection.connectionManager;
 import utilities.EmailSender;
 
-public class UserAuthentication
-{
+public class UserAuthentication {
 	private static Map<String, OtpManager> otpManager = new HashMap<String, OtpManager>();
 	private EmailSender emailSender = new EmailSender();
 	private String emailValidationPattern = "^[a-zA-Z]{1,30}[_A-Za-z0-9-]{0,30}(\\.[A-Za-z0-9-_]{1,30})*@[a-zA-Z0-9-]{1,30}(\\.[A-Za-z0-9-]{1,30}){1,2}$";
+
 	
-	public boolean generateOtpAndSendEmail(String email)
-	{
-		if(!email.matches(emailValidationPattern))
+	public static final int ACTION_INVALID_USER = 1;
+	public static final int ACTION_VALID_NEW_USER = 2;
+	public static final int ACTION_VALID_EXSISTING_USER = 3;
+
+	public boolean generateOtpAndSendEmail(String email) {
+		if (!email.matches(emailValidationPattern))
 			return false;
-		
+
 		String otp = utilities.StringTools.getRandomString(6);
 		emailSender.sendEmail(email, "Hi there, here is your OTP: " + otp + ". Complete verification process.");
 		OtpManager om = new OtpManager();
@@ -26,31 +36,105 @@ public class UserAuthentication
 		otpManager.put(email, om);
 		return true;
 	}
-	
-	public boolean verifyOtp(String email, String otp)
-	{
-		if(!email.matches(emailValidationPattern))
+
+	public boolean verifyOtp(String email, String otp) {
+		if (!email.matches(emailValidationPattern))
 			return false;
-		
-		if(otpManager.containsKey(email))
-		{
+
+		if (otpManager.containsKey(email)) {
 			OtpManager otpInCache = otpManager.get(email);
-			if(otpInCache != null)
-			{
-				boolean isOtpValid = (new Date().getTime()) - otpInCache.time > 5 * 60 * 1000 ? false : true;   
-				if(isOtpValid && otpInCache.otp.equals(otp))
-				{
+			if (otpInCache != null) {
+				boolean isOtpValid = (new Date().getTime()) - otpInCache.time > 5 * 60 * 1000 ? false : true;
+				if (isOtpValid && otpInCache.otp.equals(otp)) {
 					otpManager.remove(email);
 					return true;
 				}
 			}
 		}
-		
+
 		return false;
 	}
-	
-	private class OtpManager
-	{
+
+	public static boolean SignupUser(String email, String password) {
+
+		try {
+			int actionToTake = IsExistingUser(email);
+
+			switch (actionToTake) {
+			case ACTION_INVALID_USER:
+
+				break;
+			case ACTION_VALID_NEW_USER:
+				savePassword(email, password);
+				break;
+
+			case ACTION_VALID_EXSISTING_USER:
+
+				break;
+			default:
+				break;
+			}
+
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return false;
+	}
+
+	private static boolean savePassword(String email, String password) throws ClassNotFoundException {
+		boolean isPasswordUpdated = false;
+		String SavePassword = "UPDATE MM_USER SET USER_PASSWORD = " + password + " WHERE USER_EMAIL = " + email + "";
+		try {
+			Connection conn = connectionManager.getConnection();
+			PreparedStatement pStmt = conn.prepareStatement(SavePassword);
+			if (pStmt.execute())
+				isPasswordUpdated = true;
+			else
+				isPasswordUpdated = false;
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return isPasswordUpdated;
+	}
+
+	public static int IsExistingUser(String email) throws ClassNotFoundException {
+
+		int actionToTake = 0;
+
+		String SqlIsExistingUser = "SELECT USER_EMAIL, USER_PASSWORD FROM MM_USER WHERE USER_EMAIL = '"+email+"'";
+
+		Connection conn = connectionManager.getConnection();
+		try {
+			PreparedStatement pStmt = conn.prepareStatement(SqlIsExistingUser);
+			ResultSet resultSet = pStmt.executeQuery();
+			if (!resultSet.next()) {
+				//System.out.println("User Not allowed to access Magic Moments");
+				actionToTake = ACTION_INVALID_USER;
+			} else {
+				String password = resultSet.getString("USER_PASSWORD");
+				if (password == null) {
+					//System.out.println("new User");
+					actionToTake = ACTION_VALID_NEW_USER;
+				} else {
+					//System.out.println("Exsisting User");
+					actionToTake = ACTION_VALID_EXSISTING_USER;
+				}
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return actionToTake;
+	}
+
+	private class OtpManager {
 		String email;
 		String otp;
 		long time;
